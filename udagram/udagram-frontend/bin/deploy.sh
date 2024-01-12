ac52c845cf3fc8ece1ff292dab2871b139e989b5
Jean <jeanhepe@hotmail.com>
2024-01-12 15:25:02 +0100

    updated deploy script

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www AWS_BUCKET
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html AWS_BUCKET
+aws s3 cp ./www AWS_BUCKET --recursive --acl public-read
+aws s3 cp ./www/index.html AWS_BUCKET --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate"
