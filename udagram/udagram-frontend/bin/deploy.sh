5f400ec5100e350e310e3cff52c94179df6e0500
Jean <jeanhepe@hotmail.com>
2024-01-14 22:26:36 +0100

    changes in aws bucket reverted

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp ./www $AWS_BUCKET_FRONTEND --recursive --acl public-read
-aws s3 cp ./www/index.html $AWS_BUCKET_FRONTEND --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate"
+aws s3 cp ./www $AWS_BUCKET --recursive --acl public-read
+aws s3 cp ./www/index.html $AWS_BUCKET --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate"
